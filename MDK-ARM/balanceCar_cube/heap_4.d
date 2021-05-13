0328ebdab3a563b143d8a1769058ac91ba285cc1
SoyMilk Huang <huangxinjiang1996@gmail.com>
2021-05-14 01:02:40 +0800

    init

---
+++ MDK-ARM/balanceCar_cube/heap_4.d
@@ -0,0 +1,13 @@
+balancecar_cube\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c
+balancecar_cube\heap_4.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+balancecar_cube\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h
+balancecar_cube\heap_4.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+balancecar_cube\heap_4.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+balancecar_cube\heap_4.o: ../Core/Inc/FreeRTOSConfig.h
+balancecar_cube\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h
+balancecar_cube\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h
+balancecar_cube\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h
+balancecar_cube\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM3/portmacro.h
+balancecar_cube\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h
+balancecar_cube\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/task.h
+balancecar_cube\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h
