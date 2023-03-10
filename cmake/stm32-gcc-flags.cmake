6b0aee4cdbbd3fd031c06de74b0a77c4aa19f839
Pony Huang <huangxinjiang1996@gmail.com>
2023-03-10 15:46:26 +0800

    update cmake

--- cmake/stm32-gcc-flags.cmake
+++ cmake/stm32-gcc-flags.cmake
@@ -1,9 +1,8 @@
-
 # generate flags from user variables
 if(CMAKE_BUILD_TYPE MATCHES Debug)
-set(DBG_FLAGS "-g3 -gdwarf-2 -O0")
+    set(DBG_FLAGS "-g3 -gdwarf-2 -O0")
 elseif(CMAKE_BUILD_TYPE MATCHES Release)
-set(DBG_FLAGS "-O0")
+    set(DBG_FLAGS "-O0")
 endif()
 
 # stm32 gcc common flags
@@ -22,6 +21,10 @@ set(CMAKE_CXX_FLAGS_RELEASE "" CACHE INTERNAL "Cxx compiler flags: Release")
 set(CMAKE_ASM_FLAGS "${MCU_FLAGS} -x assembler-with-cpp ${DBG_FLAGS} " CACHE INTERNAL "ASM compiler flags")
 set(CMAKE_ASM_FLAGS_DEBUG "" CACHE INTERNAL "ASM compiler flags: Debug")
 set(CMAKE_ASM_FLAGS_RELEASE "" CACHE INTERNAL "ASM compiler flags: Release")
+
+set(MCU_FLAGS "${CPU} -mthumb ${FPU} ${FLOAT_ABI}")
+set(LINK_FLAGS "${MCU_FLAGS} -Wl,--gc-sections")
+set(EXTRA_LINK_FLAGS "-Wl,-Map=${PROJECT_NAME}.map,--cref,--no-warn-mismatch -specs=nano.specs -specs=nosys.specs")
 
 set(CMAKE_EXE_LINKER_FLAGS "${LINK_FLAGS}" CACHE INTERNAL "Exe linker flags")
 set(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS "" CACHE INTERNAL "Shared linker flags")
